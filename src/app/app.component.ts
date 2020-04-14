7078b570c719fa5bc50d3acb01e228ff3feb806b
Anthony Perez <antrp93@gmail.com>
2020-04-13 21:03:08 -0500

    displayed table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,44 @@
 import { Component } from '@angular/core';
 import { IGameData } from './interfaces/igame-data';
-import { MatTable } from '@angular/material/table';
-
-const GAME_DATA: IGameData[] = [
-  {
-    id: 1,
-    title: 'Rocket League',
-    developer: 'Psyonix',
-    dateCreated: new Date(),
-  },
-  {
-    id: 2,
-    title: 'Doom',
-    developer: 'id Software',
-    dateCreated: new Date(),
-  },
-  {
-    id: 3,
-    title: 'Undertale',
-    developer: 'Toby Fox',
-    dateCreated: new Date(),
-  },
-  {
-    id: 4,
-    title: 'Fallout: New Vegas',
-    developer: 'Obsidian Studios',
-    dateCreated: new Date(),
-  },
-  {
-    id: 5,
-    title: 'Portal 2',
-    developer: 'Valve',
-    dateCreated: new Date(),
-  }
-]
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-  title = 'MatTable';
+  GAME_DATA: IGameData[] = [
+    {
+      id: 1,
+      title: 'Rocket League',
+      developer: 'Psyonix',
+      dateCreated: new Date(),
+    },
+    {
+      id: 2,
+      title: 'Doom',
+      developer: 'id Software',
+      dateCreated: new Date(),
+    },
+    {
+      id: 3,
+      title: 'Undertale',
+      developer: 'Toby Fox',
+      dateCreated: new Date(),
+    },
+    {
+      id: 4,
+      title: 'Fallout: New Vegas',
+      developer: 'Obsidian Studios',
+      dateCreated: new Date(),
+    },
+    {
+      id: 5,
+      title: 'Portal 2',
+      developer: 'Valve',
+      dateCreated: new Date(),
+    },
+  ];
+  displayedColumns: string[] = ['id', 'title', 'developer', 'dateCreated'];
+  dataSource = this.GAME_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MatTableModule} from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
