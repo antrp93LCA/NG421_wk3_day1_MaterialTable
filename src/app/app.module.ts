12d8d8ba6356989519347675ef8bbba9cab895b4
Anthony Perez <antrp93@gmail.com>
2020-04-14 21:35:16 -0500

    fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import {MatSortModule} from '@angular/material';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GameTableComponent } from './game-table/game-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameTableComponent
   ],
   imports: [
     BrowserModule,
